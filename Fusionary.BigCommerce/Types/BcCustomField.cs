fd1a0cc8a6864e934ed6bd966b264040b17e21a7
Jeremy Bell <grcodemonkey@gmail.com>
2023-08-01 01:00:18 -0400

    Add ignore condition for CustomField.Id

--- Fusionary.BigCommerce/Types/BcCustomField.cs
+++ Fusionary.BigCommerce/Types/BcCustomField.cs
@@ -1,7 +1,13 @@
 namespace Fusionary.BigCommerce.Types;
 
-[DebuggerDisplay("{Name}:{Value} ({Id})")]
+[DebuggerDisplay("{Name}:{Value}")]
 public record BcCustomField : BcCustomFieldPost
 {
-    
+    /// <summary>
+    /// The unique numeric ID of the custom field; increments sequentially.
+    /// Required to update existing custom field in /PUT request. Read-Only
+    /// </summary>
+    [JsonPropertyName("id")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public new int Id { get; set; }
 }
