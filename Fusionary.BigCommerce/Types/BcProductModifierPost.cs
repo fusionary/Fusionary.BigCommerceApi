5e8f1a38cf73d2c27d5c902daab499630dcf9aaf
Richard Keller <richard@1800battery.com>
2024-08-17 12:57:06 -0700

    Update Empty Value Converter

--- Fusionary.BigCommerce/Types/BcProductModifierPost.cs
+++ Fusionary.BigCommerce/Types/BcProductModifierPost.cs
@@ -13,6 +13,7 @@ public record BcProductModifierPost : IExtensionData
 
     public bool Required { get; set; }
 
+    [JsonConverter(typeof(EmptyArrayToObjectConverter<BcOptionConfig>))]
     public BcOptionConfig? Config { get; set; }
 
     public List<BcModifierOptionValue>? OptionValues { get; set; }
@@ -20,6 +21,40 @@ public record BcProductModifierPost : IExtensionData
 
     public int? SharedOptionId { get; set; }
     [JsonPropertyName("shared_option_id")]
+
+    public class EmptyArrayToObjectConverter<T> : JsonConverter<T?>
+    where T : class
+    {
+        public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // If the token is an empty array, return null
+            if (reader.TokenType == JsonTokenType.StartArray)
+            {
+                reader.Read(); // Move past the start of the array
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return null; // Return null if the array is empty
+                }
+                throw new JsonException("Expected empty array or object");
+            }
+
+            // Otherwise, deserialize the object normally
+            return JsonSerializer.Deserialize<T>(ref reader, options);
+        }
+
+        public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
+        {
+            if (value == null)
+            {
+                writer.WriteStartArray();
+                writer.WriteEndArray();
+            }
+            else
+            {
+                JsonSerializer.Serialize(writer, value, options);
+            }
+        }
+    }
 
     /// <inheritdoc />
     public IDictionary<string, JsonElement>? ExtensionData { get; init; }
