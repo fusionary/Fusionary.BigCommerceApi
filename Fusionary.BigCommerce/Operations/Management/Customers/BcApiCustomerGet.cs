2276461dff6f92f288fd0cfa04a3ad9be7cf4071
Eric Petroelje <eric.petroelje@fusionary.com>
2025-08-06 09:39:50 -0400

    Added get all customers endpoint.

--- Fusionary.BigCommerce/Operations/Management/Customers/BcApiCustomerGet.cs
+++ Fusionary.BigCommerce/Operations/Management/Customers/BcApiCustomerGet.cs
@@ -3,6 +3,22 @@ namespace Fusionary.BigCommerce.Operations;
 public class BcApiCustomerGet(IBcApi api) : BcRequestBuilder(api), IBcApiOperation
 {
     public Task<BcResultPaged<BcCustomer>> SendAsync(
+        CancellationToken cancellationToken = default
+    ) => SendAsync<BcCustomer>(cancellationToken);
+
+    public async Task<BcResultPaged<T>> SendAsync<T>(
+        CancellationToken cancellationToken = default
+    )
+    {
+        return await Api.GetPagedAsync<T>(
+            BcEndpoint.CustomersV3(),
+            Filter,
+            Options,
+            cancellationToken
+        );
+    }
+    
+    public Task<BcResultPaged<BcCustomer>> SendAsync(
         int id,
         CancellationToken cancellationToken = default
     ) => SendAsync<BcCustomer>(id, cancellationToken);
