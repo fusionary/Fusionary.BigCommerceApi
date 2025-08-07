fe1fd5690205fd18472a50f22867b7b14ac64a04
Eric Petroelje <eric.petroelje@fusionary.com>
2025-08-07 10:27:32 -0400

    Customer create and update endpoints.

--- Fusionary.BigCommerce/Operations/Management/Customers/BcApiCustomer.cs
+++ Fusionary.BigCommerce/Operations/Management/Customers/BcApiCustomer.cs
@@ -3,4 +3,8 @@ namespace Fusionary.BigCommerce.Operations;
 public class BcApiCustomer(IBcApi api) : IBcApiOperation
 {
     public BcApiCustomerGet Get() => new(api);
+    
+    public BcApiCustomerCreate Create() => new(api);
+    
+    public BcApiCustomerUpdate Update() => new(api);
 }
---
+++ Fusionary.BigCommerce/Operations/Management/Customers/BcApiCustomerUpdate.cs
@@ -0,0 +1,10 @@
+﻿namespace Fusionary.BigCommerce.Operations;
+
+public class BcApiCustomerUpdate(IBcApi api) : BcRequestBuilder(api), IBcApiOperation
+{
+    public Task<BcResultData<BcCustomer>> SendAsync(BcCustomer customer, CancellationToken cancellationToken = default)
+        => SendAsync<BcCustomer>(customer, cancellationToken);
+
+    public Task<BcResultData<T>> SendAsync<T>(object customer, CancellationToken cancellationToken = default)
+        => Api.PutDataAsync<T>(BcEndpoint.CustomersV3(), customer, Filter, Options, cancellationToken);
+}
