5316801c3cca436bf55f51cf7e9fb8f261b1993b
Eric Petroelje <eric.petroelje@fusionary.com>
2025-08-13 14:46:34 -0400

    Added methods to get pricelist records and delete them.

---
+++ Fusionary.BigCommerce/Operations/Management/Price Lists/Price Lists Records/BcApiPriceListRecordGetAll.cs
@@ -0,0 +1,15 @@
+﻿namespace Fusionary.BigCommerce.Operations;
+
+public class BcApiPriceListRecordGetAll(IBcApi api) : BcRequestBuilder(api), IBcPageableFilter, IBcApiOperation
+{
+    public Task<BcResultPaged<BcPriceListRecord>> SendAsync(BcId priceListId, CancellationToken cancellationToken = default) =>
+        SendAsync<BcPriceListRecord>(priceListId, cancellationToken);
+
+    public async Task<BcResultPaged<T>> SendAsync<T>(BcId priceListId, CancellationToken cancellationToken = default) =>
+        await Api.GetPagedAsync<T>(
+            BcEndpoint.PriceListRecordsV3(priceListId),
+            Filter,
+            Options,
+            cancellationToken
+        );    
+}
--- Fusionary.BigCommerce/Operations/Management/Price Lists/Price Lists Records/BcApiPriceListRecords.cs
+++ Fusionary.BigCommerce/Operations/Management/Price Lists/Price Lists Records/BcApiPriceListRecords.cs
@@ -3,4 +3,19 @@ namespace Fusionary.BigCommerce.Operations;
 public class BcApiPriceListRecords(IBcApi api) : IBcApiOperation
 {
     public BcApiPriceListRecordUpsert Upsert() => new(api);
+
+    public BcApiPriceListRecordGetAll GetAll() => new(api);
+
+    public BcApiPriceListRecordsDeleteAll DeleteAll() => new(api);
+}
+
+public class BcApiPriceListRecordsDeleteAll(IBcApi api) : BcRequestBuilder(api), IBcApiOperation, IBcSkuFilter
+{
+    public async Task<BcResult> SendAsync(BcId priceListId, CancellationToken cancellationToken = default) =>
+        await Api.DeleteAsync(
+            BcEndpoint.PriceListRecordsV3(priceListId),
+            Filter,
+            Options,
+            cancellationToken
+        );
 }
